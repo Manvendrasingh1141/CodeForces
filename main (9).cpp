a4688ab9dfc57e8ce40d54e2fa3c6c0be986d4ba
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-05-11 23:33:06 +0530

    Add files via upload

---
+++ main (9).cpp
@@ -0,0 +1,50 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        ans.push_back(v[0]);
+        for(int i=1;i<n;i++){
+            if(v[i]>=v[i-1])ans.push_back(v[i]);
+            else{
+                ans.push_back(v[i]);
+                ans.push_back(v[i]);
+            }
+        }
+        cout<<ans.size()<<"\n";
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
