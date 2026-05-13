b06023d91a0229e1316766e77c35908f8d2ecc84
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-05-13 21:39:08 +0530

    Add files via upload

---
+++ main (12).cpp
@@ -0,0 +1,48 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,s=0;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        vector<int>check=v;
+        sort(check.begin(),check.end());
+        
+        int a=v[n-1];
+        int b=v[n-2];
+        while(b<=a){
+            s++;
+            b++;
+            a--;
+        }
+        cout<<abs(s)<<"\n";
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
