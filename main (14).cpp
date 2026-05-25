e47425990d4de645c0c3c7af0ed19d979c832464
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-05-25 23:43:13 +0530

    Add files via upload

---
+++ main (14).cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for(int i = 0; i < n; i++) cin >> a[i];
+
+        int ans = INT_MAX;
+
+        for(int i = 0; i < n - 1; i++) {
+            if(a[i] > a[i + 1]) {
+                ans = 0;
+                break;
+            }
+            ans = min(ans, (a[i + 1] - a[i]) / 2 + 1);
+        }
+
+        cout << ans << "\n";
+    }
+}
